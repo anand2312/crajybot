17627d478d9799d21f7d1377da10425c30e1f018
Unknown <anandkrishna2312@gmail.com>
2020-05-11 18:21:30 +0400

    Start!
    imported discord.py; made the client and connected to the bot. the bot will be online when this file is running.

---
+++ botTrial.py
@@ -0,0 +1,12 @@
+"""
+Horoscopebot
+insert further documentation here
+
+"""
+import os
+import discord
+token = "NzA5NDA3MjY4NDg3MDM3MDE5.XrldCQ.Up2M6F_CgEzg_hGrXU76XEapMxg"
+client =  discord.Client()
+client.run(token)
+
+
