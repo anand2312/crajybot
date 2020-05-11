e29ed9c2552c1062948a682346bf8fe54e20595e
Unknown <anandkrishna2312@gmail.com>
2020-05-11 19:24:34 +0400

    added test event
    popi -- returns straight facts

--- botTrial.py
+++ botTrial.py
@@ -5,9 +5,18 @@ insert further documentation here
 """
 import os
 import discord
+
 token_ = "NzA5NDA3MjY4NDg3MDM3MDE5.XrllLQ.FbL2vivvNxjxPT-wOfAvH32fK4QZ"
 token = token_[:len(token_)-1]
+
 client =  discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content == "popi":
+        await message.channel.send("poopi really do be poopie tho")
+
+
 client.run(token)
 
 
