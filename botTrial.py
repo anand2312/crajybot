88c84a3e23bce42de72a6665547d707ff04e85ae
Rishabh69420 <65121903+Rishabh69420@users.noreply.github.com>
2020-05-11 19:33:58 +0400

    Update botTrial.py

--- botTrial.py
+++ botTrial.py
@@ -13,7 +13,7 @@ client =  discord.Client()
 
 @client.event
 async def on_message(message):
-    if message.content == "popi":
+    if message.content == "popi" or "Popi":
         await message.channel.send("poopi do be lookin poopie")
 
 
