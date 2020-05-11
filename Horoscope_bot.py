51b4d485eb1bfae8f64cd88c741dca37144c6143
Rishabh69420 <65121903+Rishabh69420@users.noreply.github.com>
2020-05-11 08:38:24 +0400

    Update Horoscope_bot.py

--- Horoscope_bot.py
+++ Horoscope_bot.py
@@ -3,3 +3,21 @@
 #dmamb
 
 print("Popi do be lookin kinda poopi")
+
+class nigga(self):
+  
+  def __init__(self,first,last):
+    self.first=first
+    self.last=last
+  
+  def full_name:
+    f_name="{} {}".format(self.first,self.last)
+    
+  def is_nigga(self):
+    if f_name="Shriram Shekar":
+      return True
+
+user1=nigga("Shriram","Shekar")
+user1.full_name()
+if user1.is__nigga:
+  print("popi")
