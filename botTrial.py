5c7d0882a492ae95f89d02c04c8dc9eaab5c38b4
Unknown <moonlight.shriram@gmail.com>
2020-05-13 19:37:41 +0400

    BotTrial Code cleaned and organized

--- botTrial.py
+++ botTrial.py
@@ -4,77 +4,71 @@ insert further documentation here, insert documentation near new functions or va
 
 """
 import os
-import discord
 import random
+import discord 
+from discord.ext import commands
 from pymongo import MongoClient
 
 #mongodb initialization for economy stats
-client = MongoClient("mongodb+srv://bot-dev:linusgun@cluster0-efytb.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://shriram:a1s2d3f4g5@cluster0-efytb.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = client.botuserdata
-
 
 #bot token stuff; not to be messed with :linus_gun:
 token_ = "NzA5NDA3MjY4NDg3MDM3MDE5.XrllLQ.FbL2vivvNxjxPT-wOfAvH32fK4QZ"
 token = token_[:len(token_)-1]
 
-client =  discord.Client()
-@client.event
-async def on_ready():    #sends this message when bot starts working in #bot-tests
-    await client.get_channel(703141348131471440).send("I'm ready to fuck your mom.")
+#bot which controls everything; subclass of Client
+bot = commands.Bot(command_prefix='$')
 
-@client.event
-async def on_message(message):
-    guild_id = client.get_guild(298871492924669954) #server ID variable that gets info from our server
-    channels = ["bot-test","botspam-v2","botspam"]  #limiting bot usage to these channels
+channels_available = ["bot-test","botspam-v2","botspam"] #Channels where the bot works
+bot.remove_command('help') 
+#Lets us implement our own help command instead of the built in one
 
-    if str(message.channel) in channels:
-        if message.content[0] == "$":
-            if message.content.lower() == "$members":  #returns number of users
-                emb1 = discord.Embed(title = "Members" , description = f"""Number of members = {guild_id.member_count}""")
-                await message.channel.send(content = None, embed =  emb1)
+@bot.event
+async def on_ready(): #sends this message when bot starts working in #bot-tests
+    await bot.get_channel(703141348131471440).send("I'm ready to fuck your mom.")
 
-            elif message.content.lower() == "$popi":   #popi
-                n = random.randint(1,2)
-                if n == 1:
-                    emb2 = discord.Embed(title = "popi" , description = f"poopi really do be poopie though")
-                    await message.channel.send(content = None, embed = emb2)
-                elif n == 2:
-                    emb2 = discord.Embed(title = "popi" , description = f"{message.author.mention} is a poopie?oh no......")
-                    await message.channel.send(content = None, embed = emb2)
+#ctx stands for context
+@bot.command(name='members') #name of the command $members
+async def returnMembers(ctx):
+    membercount = ctx.guild.member_count #guild refers to the server.
+    response = discord.Embed(title="Members" , description=f"""Number of members = {membercount}""") #Embed is what displays it in a box thing (I think)
+    if ctx.message.channel.name in channels_available: await ctx.message.channel.send(content=None,embed=response)
 
-            elif message.content.lower() == "$sexrating":   #returns random sex rating value b/w 1 and 5
-                emb3 = discord.Embed(title = "Sexrating" , description = f"{random.randint(1,5)} {client.get_emoji(703648807271006379)}" )  #creating embded, lookin saxy
-                await message.channel.send(content = None, embed =  emb3)
+@bot.command(name='popi')
+async def popi(ctx):
+    reply = random.choice(["poopi really do be poopie though",f"{ctx.message.author.mention} is a poopie?oh no......"]) #Choice chooses 1 object from the list
+    response = discord.Embed(title='popi',description=reply)
+    if ctx.message.channel.name in channels_available: await ctx.message.channel.send(content=None,embed=response)
 
-            elif message.content.lower() == "$help":  #help command
-                emb4 = discord.Embed(title = "Help" , description = "List of commands")
-                #adding fields to embed 
-                emb4.add_field(name = "$popi", value = "popi", inline = False)
-                emb4.add_field(name = "$members", value = "Returns number of members in server", inline = False)
-                emb4.add_field(name = "$sexrating", value = f"Are you at your sexual peak today? {client.get_emoji(708951950162395166)}", inline = False)
+@bot.command(name='help')
+async def help(ctx):
+    response = discord.Embed(title='Help',description='List of commands')
+    response.add_field(name="$popi", value="popi", inline = False)
+    response.add_field(name="$members", value="Returns number of members in server", inline = False)
+    if ctx.message.channel.name in channels_available: await ctx.message.channel.send(content=None,embed=response)
 
-                await message.channel.send(content = None, embed = emb4 )
+#ECONOMY CODE; ADD SPACE ABOVE THIS FOR OTHER UNRELATED FUNCTIONS PLEASE :linus_gun:
 
+@bot.command(name='bal')
+async def balance(ctx):
+    data_bal = db.economyvalues.find_one({"name":str(ctx.message.author)})
+    response = discord.Embed(title=str(ctx.message.author), description=f"Bank balance : {data_bal['bal']}")
+    if ctx.message.channel.name in channels_available: await ctx.message.channel.send(content=None,embed=response)
 
-            #ECONOMY CODE; ADD SPACE ABOVE THIS FOR OTHER UNRELATED FUNCTIONS PLEASE :linus_gun:
+@bot.command(name='work')
+async def work(ctx):
+    data_earn = db.economyvalues.find_one({"name":str(ctx.message.author)})
+    curr_bal = data_earn["bal"]
+    rand_val = random.randint(35,150)
+    
+    response = discord.Embed(title=str(ctx.message.author),description=f"You earned {rand_val}",colour=discord.Colour.green())
 
-            elif message.content.lower() == "$bal":
-                data_bal = db.economyvalues.find_one({"name":str(message.author)})
-                emb5 = discord.Embed(title = str(message.author), description = f"Bank balance : {data_bal['bal']}")
-                await message.channel.send(content = None, embed = emb5)
-                
-            elif message.content.lower() == "$work":   #just test commands, we can change the names later
-                data_earn = db.economyvalues.find_one({"name":str(message.author)})
-                curr_bal = data_earn["bal"]
-                rand_val = random.randint(35,150)
-
-                embed = discord.Embed(title = str(message.author), description = f"You earned {rand_val}", colour = discord.Colour.green())
-
-                db.economyvalues.update_one({"name":str(message.author)} , {'$set':{"bal" : rand_val + curr_bal}})
-                await message.channel.send(content = None, embed = embed)
-            
+    db.economyvalues.update_one({"name":str(ctx.message.author)} , {'$set':{"bal" : rand_val + curr_bal}})
+ 
+    if ctx.message.channel.name in channels_available: await ctx.message.channel.send(content=None,embed=response)
 
 
-client.run(token)
+bot.run(token)
 
 
