300989de54903270a45f35501a9aba0aff14c604
AbsoluteMadlad <anandkrishna2312@gmail.com>
2020-05-15 12:20:13 +0400

    Git poopied last time i commited this

--- economy-code.py
+++ economy-code.py
@@ -1,9 +1,3 @@
-'''from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://bot-dev:linusgun@horoscope-bot-efytb.mongodb.net/test?retryWrites=true&w=majority")
-db = client.botuserdata
-d = db.economyvalues.find_one({"name":"Anand"})
-print(d)'''
 import json
 econ_stats =[
     {
@@ -17,6 +11,94 @@ econ_stats =[
     },
     {
         "user" : "Sir Poopy Pants#0136",
+        "bal" : 0,
+        "inv" : [
+            {"stock" : 0}
+        ],
+        "debt" : 0
+    },
+    {
+        "user" : "Foxwid#8333",
+        "bal" : 0,
+        "inv" : [
+            {"stock" : 0}
+        ],
+        "debt" : 0
+    },
+    {
+        "user" : "MANDARIN#8024",
+        "bal" : 0,
+        "inv" : [
+            {"stock" : 0}
+        ],
+        "debt" : 0
+    },
+    {
+        "user" : "humanboiii#5837",
+        "bal" : 0,
+        "inv" : [
+            {"stock" : 0}
+        ],
+        "debt" : 0
+    },
+    {
+        "user" : "Windsmith#4272",
+        "bal" : 0,
+        "inv" : [
+            {"stock" : 0}
+        ],
+        "debt" : 0
+    },
+    {
+        "user" : "BOOOZ#4084",
+        "bal" : 0,
+        "inv" : [
+            {"stock" : 0}
+        ],
+        "debt" : 0
+    },
+    {
+        "user" : "CrashGamer#7883",
+        "bal" : 0,
+        "inv" : [
+            {"stock" : 0}
+        ],
+        "debt" : 0
+    },
+    {
+        "user" : "maafi_username#7152",
+        "bal" : 0,
+        "inv" : [
+            {"stock" : 0}
+        ],
+        "debt" : 0
+    },
+    {
+        "user" : "prakhar5723#3126",
+        "bal" : 0,
+        "inv" : [
+            {"stock" : 0}
+        ],
+        "debt" : 0
+    },
+    {
+        "user" : "smelly farts#9535",
+        "bal" : 0,
+        "inv" : [
+            {"stock" : 0}
+        ],
+        "debt" : 0
+    },
+    {
+        "user" : "Sugeeth2401#3350",
+        "bal" : 0,
+        "inv" : [
+            {"stock" : 0}
+        ],
+        "debt" : 0
+    },
+    {
+        "user" : "shreyash0368#2025",
         "bal" : 0,
         "inv" : [
             {"stock" : 0}
