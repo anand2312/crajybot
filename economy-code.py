2b5748d6342288a1404e8943e834e358583b2cea
Unknown <moonlight.shriram@gmail.com>
2020-06-18 11:32:33 +0400

    Made economycode similar to the one on master.

--- economy-code.py
+++ economy-code.py
@@ -2,121 +2,204 @@ import json
 econ_stats =[
     {
         "user" : "Ares#7286",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
 
     },
     {
         "user" : "Sir Poopy Pants#0136",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
     },
     {
         "user" : "Foxwid#8333",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
     },
     {
         "user" : "MANDARIN#8024",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
     },
     {
         "user" : "humanboiii#5837",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
     },
     {
         "user" : "Windsmith#4272",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
     },
     {
         "user" : "BOOOZ#4084",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
     },
     {
         "user" : "CrashGamer#7883",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
     },
     {
         "user" : "maafi_username#7152",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
     },
     {
         "user" : "prakhar5723#3126",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
     },
     {
         "user" : "smelly farts#9535",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
     },
     {
         "user" : "Sugeeth2401#3350",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 0,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
-        "debt" : 0,
+        "debt" : 0
         
     },
     {
         "user" : "shreyash0368#2025",
-        "bal" : 0,
+        "cash" : 0,
+        "bank" : 2500,
         "inv" : [
-            {"stock" : 0}
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
         ],
         "debt" : 0,
-        
+
+        "zodiac_sign" : ""
+    },
+    {
+        "user" : "Jackson Williams#4755",
+        "cash" : 0,
+        "bank" : 2500,
+        "inv" : [
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
+        ],
+        "debt" : 0,
+        "zodiac_sign" : ""
+    },
+    {
+        "user" : "kaanank90#4243",
+        "cash" : 0,
+        "bank" : 2500,
+        "inv" : [
+            {"stock" : 0},
+            {"chicken" : 0},
+            {"heist tools" : 0}
+        ],
+        "debt" : 0,
+        "zodiac_sign" : ""
+    }
+]
+
+store_data = [
+    {
+        "name" : "Stock",
+        "price" : 25,
+        "stock" :10000
+    },
+    {
+        "name" : "Chicken",
+        "price" : 100,
+        "stock" : None
+    },
+    {
+        "name" : "Heist tools",
+        "price" : 250,
+        "stock" : None
+
     }
 ]
 
@@ -242,13 +325,6 @@ rpg_data = [
     }
 ]
 
-store_data = [
-    {
-        "name" : "Stock",
-        "price" : 25,
-        "stock" : 10000
-    }
-]
 
 with open("economy-data.json","w+") as data:
     json.dump(econ_stats,data)
