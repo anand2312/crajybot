6985f1d526240671dc219b8ef75d33b3bdc95c21
Anand Krishna <anandkrishna2312@gmail.com>
2020-09-25 13:06:43 +0400

    Rewrite part-1
    - Code rewritten to work with user IDs being stored in database till the `repay-loan` command.
    - Code also cleaned up a bit to be more PEP8 compliant.
    - Made a cog check that does the same as the 
    `if ctx.channel.name in channels_available` 
    check we were doing manually.

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from pymongo import MongoClient
+import typing
 import asyncio
 import random
 
@@ -10,68 +11,75 @@ db = client["bot-data"]
 economy_collection = db["econ_data"]
 store_collection = db["store_data"]
 
-channels_available = ["bot-test","botspam-v2","botspam"]
-
 class Economy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name = "withdraw", aliases = ["with"])
-    async def withdraw(self,ctx,amount):
+    async def cog_check(self, ctx):
+        return ctx.channel.name in ["bot-test","botspam-v2","botspam"]
+
+    @commands.command(name="withdraw", aliases=["with"])
+    async def withdraw(self, ctx, amount):
         try:
             amount = int(amount)
-            if economy_collection.find_one({'user':str(ctx.message.author)})['bank'] >= amount:
-                economy_collection.find_one_and_update({'user':str(ctx.message.author)},{"$inc":{'bank': -amount, 'cash':amount}})
+            if economy_collection.find_one({'user': ctx.author.id})['bank'] >= amount:
+                economy_collection.update_one({'user': ctx.author.id},{"$inc": {'bank': -amount, 'cash':amount}})
                 response = discord.Embed(title = str(ctx.message.author), description = f"Withdrew {amount}", colour = discord.Color.green())
-                await ctx.message.channel.send(content = None, embed = response)
+                await ctx.message.channel.send(embed=response)
             else:
                 await ctx.message.channel.send("You do not have that much balance")
         except ValueError:
             if amount.lower() == "all":
-                user_data = economy_collection.find_one({'user':str(ctx.message.author)})
-                economy_collection.find_one_and_update({'user':str(ctx.message.author)},{"$set":{'bank':0, 'cash':user_data['cash']+user_data['bank']}})
+                user_data = economy_collection.find_one({'user': ctx.author.id)
+                economy_collection.find_one_and_update({'user': ctx.author.id}, {"$set":{'bank': 0, 'cash': user_data['cash']+user_data['bank']}})
                 response = discord.Embed(title = str(ctx.message.author), description = f"Withdrew {user_data['bank']}", colour = discord.Color.green())
-                await ctx.message.channel.send(content = None, embed = response)
+                await ctx.message.channel.send(embed=response)
 
-    @commands.command(name = "deposit", aliases = ["dep"])
-    async def deposit(self,ctx,amount):
+    @commands.command(name="deposit", aliases=["dep"])
+    async def deposit(self, ctx, amount):
             try:
                 amount = int(amount)
-                user_data = economy_collection.find_one({'user':str(ctx.message.author)})
+                user_data = economy_collection.find_one({'user': ctx.author.id})
                 if user_data['cash'] >= amount:
-                    economy_collection.find_one_and_update({'user':str(ctx.message.author)}, {"$inc":{'cash': -amount, 'bank': amount}})
+                    economy_collection.update_one({'user': ctx.author.id}, {"$inc":{'cash': -amount, 'bank': amount}})
                     response = discord.Embed(title = str(ctx.message.author), description = f"Deposited {amount}", colour = discord.Color.green())
-                    await ctx.message.channel.send(content = None, embed = response)
+                    await ctx.message.channel.send(embed=response)
                 else:
                     await ctx.message.channel.send("You don't have that much moni to deposit")
             except ValueError:
                 if amount.lower() == "all":
-                    user_data = economy_collection.find_one({'user':str(ctx.message.author)})
-                    economy_collection.find_one_and_update({'user':str(ctx.message.author)}, {"$inc":{'cash': -user_data['cash'], 'bank': user_data['cash']}})
+                    user_data = economy_collection.find_one({'user': ctx.author.id})
+                    economy_collection.update_one({'user': ctx.author.id}, {"$inc":{'cash': -user_data['cash'], 'bank': user_data['cash']}})
                     response = discord.Embed(title = str(ctx.message.author), description = f"Deposited {user_data['cash']}", colour = discord.Color.green())
-                    await ctx.message.channel.send(content = None, embed = response)
+                    await ctx.message.channel.send(embed=response)
                     
     @commands.command(name='bal')
-    async def balance(self,ctx,user:discord.Member=None):
-        if user is not None: user_dict = economy_collection.find_one({'user':str(user)})
-        else: user_dict = economy_collection.find_one({'user':str(ctx.message.author)})
+    async def balance(self, ctx, user: typing.Union[discord.Member, str]=None):
+        if user is not None:
+            if isinstance(user, discord.Member): 
+                user_dict = economy_collection.find_one({'user': user.id})
+            elif isinstance(user, str):
+                user = [member for member in ctx.guild.members if member.name.lower()==user.lower() or member.nick.lower()==user.lower()]  #this is a list, use user[0]!
+                user_dict = economy_collection.find_one({'user': user[0].id})
+        else: 
+            user_dict = economy_collection.find_one({'user': ctx.author.id})
+
         networth = (user_dict['cash'] + user_dict['bank']) - user_dict['debt']
-        
         response = discord.Embed(title=user_dict["user"], description="Balance is:")
         response.add_field(name="Cash Balance : ",value=f"{user_dict['cash']}", inline = False)
         response.add_field(name="Bank balance : ",value=f"{user_dict['bank']}", inline = False)
         response.add_field(name="Debt : ",value=f"{-user_dict['debt']}", inline = False)
         response.add_field(name="Net Worth : ",value=networth, inline = False)
-                        #all this is for 1) in Bank, 2) Debt, 3) Total bal
-        if ctx.message.channel.name in channels_available: await ctx.message.channel.send(content=None,embed=response)
+
+        return await ctx.message.channel.send(embed=response)
 
     @commands.command(name='work')
-    @commands.cooldown(1, 3600, commands.BucketType.user)            #remember to increase the cooldown to at least an hour!
-    async def work(self,ctx):
+    @commands.cooldown(1, 3600, commands.BucketType.user)  
+    async def work(self, ctx):
         rand_val = random.randint(50,200)
-        economy_collection.find_one_and_update({'user':str(ctx.message.author)}, {"$inc":{'cash':rand_val}})
-        response = discord.Embed(title=str(ctx.message.author),description=f"You earned {rand_val}",colour=discord.Colour.green())
-        if ctx.message.channel.name in channels_available: await ctx.message.channel.send(content=None,embed=response)
+        economy_collection.update_one({'user': ctx.author.id}, {"$inc": {'cash': rand_val}})
+        response = discord.Embed(title=str(ctx.message.author), description=f"You earned {rand_val}", colour=discord.Colour.green())
+        return await ctx.message.channel.send(embed=response)
 
     @work.error
     async def work_error(self,ctx,error):             
@@ -79,47 +87,47 @@ class Economy(commands.Cog):
             await ctx.message.channel.send(f"Time remaining = {int(error.retry_after/60)} mins")
 
     @commands.command(name='slut')
-    @commands.cooldown(1, 3600, commands.BucketType.user)            #remember to increase the cooldown to at least an hour!
-    async def slut(self,ctx):
+    @commands.cooldown(1, 3600, commands.BucketType.user)          
+    async def slut(self, ctx):
         winning_odds=[1,2,3,4,5,6]
         if random.randint(1,10) in winning_odds:
             rand_val = random.randint(60,200)
-            economy_collection.update({'user':str(ctx.message.author)},{"$inc":{'cash':rand_val}})
-            response = discord.Embed(title=str(ctx.message.author),description=f"You whored out and earned {rand_val}!",colour=discord.Colour.green())
+            economy_collection.update({'user': ctx.author.id}, {"$inc": {'cash': rand_val}})
+            response = discord.Embed(title=str(ctx.message.author), description=f"You whored out and earned {rand_val}!", colour=discord.Colour.green())
         
         else:
             rand_val = random.randint(60,200)
-            economy_collection.update({'user':str(ctx.message.author)},{"$inc":{'cash':-rand_val}})
+            economy_collection.update({'user': ctx.author.id}, {"$inc": {'cash': -rand_val}})
             response = discord.Embed(title=str(ctx.message.author),description=f"You hooked up with a psychopath lost {rand_val}!",colour=discord.Colour.red())
-        if ctx.message.channel.name in channels_available: await ctx.message.channel.send(content=None,embed=response)
+        return await ctx.message.channel.send(embed=response)
     
     @slut.error
-    async def slut_error(self,ctx,error):             #only says "CommandOnCooldown", not the time remaining
+    async def slut_error(self,ctx,error):   
         if isinstance(error, commands.CommandOnCooldown):
             await ctx.message.channel.send(f"Time remaining = {int(error.retry_after/60)} mins")
 
     @commands.command(name="crime")
-    @commands.cooldown(1, 3600, commands.BucketType.user)            #remember to increase the cooldown to at least an hour!
-    async def crime(self,ctx):
+    @commands.cooldown(1, 3600, commands.BucketType.user)            
         winning_odds=[1,2,3,4]
         if random.randint(1,10) in winning_odds:
             rand_val = random.randint(150,400)
-            economy_collection.update({'user':str(ctx.message.author)}, {"$inc":{'cash':rand_val}})
-            response = discord.Embed(title=str(ctx.message.author),description=f"You successfuly commited crime and earned {rand_val}!",colour=discord.Colour.green())
+            economy_collection.update({'user': ctx.author.id}, {"$inc": {'cash': rand_val}})
+            response = discord.Embed(title=str(ctx.message.author), description=f"You successfuly commited crime and earned {rand_val}!", colour=discord.Colour.green())
         else:
             rand_val = random.randint(150,250)
-            economy_collection.update({'user':str(ctx.message.author)}, {"$inc":{'cash':-rand_val}})
-            response = discord.Embed(title=str(ctx.message.author),description=f"You got caught and were fined {rand_val}!",colour=discord.Colour.red())
-        if ctx.message.channel.name in channels_available: await ctx.message.channel.send(content=None,embed=response)
+            economy_collection.update({'user': ctx.author.id}, {"$inc": {'cash': -rand_val}})
+            response = discord.Embed(title=str(ctx.message.author), description=f"You got caught and were fined {rand_val}!", colour=discord.Colour.red())
+        return await ctx.message.channel.send(embed=response)
 
     @crime.error
-    async def crime_error(self,ctx,error):             #only says "CommandOnCooldown", not the time remaining
+    async def crime_error(self,ctx,error):            
         if isinstance(error, commands.CommandOnCooldown):
             await ctx.message.channel.send(f"Time remaining = {int(error.retry_after/60)} mins")
 
-    @commands.command(name = "leaderboard", aliases = ["top","lb"])   #unsure if this works
+    @commands.command(name="leaderboard", aliases=["top","lb"])   
     async def leaderboard(self,ctx):
         leaderboard_data = list(economy_collection.find())
+        #Bubble sort inefficient.
         for i in range(0,len(leaderboard_data)):
             for j in range(0,len(leaderboard_data) - 1 - i):
                 if leaderboard_data[j]["bank"] + leaderboard_data[j]["cash"] > leaderboard_data[j + 1]["bank"] + leaderboard_data[j + 1]["cash"]:
@@ -127,42 +135,49 @@ class Economy(commands.Cog):
 
         leaderboard_data.reverse()
 
-        response = discord.Embed(title = "Crajy Leaderboard", description = "")
+        response = discord.Embed(title="Crajy Leaderboard", description="")
         for i in leaderboard_data:
-            response.add_field(name = f"{leaderboard_data.index(i)+1}. {i['user']}", value = f"Balance {i['bank'] + i['cash']}", inline = False)
-        if ctx.message.channel.name in channels_available: await ctx.message.channel.send(content = None, embed = response)
+            person = ctx.guild.get_member(i['user'])
+            response.add_field(name=f"{leaderboard_data.index(i)+1}. {person.nick}", value=f"Balance {i['bank'] + i['cash']}", inline=False)
+        return await ctx.message.channel.send(embed=response)
 
-    @commands.command(name="get-loan", aliases = ["gl"])    #repayment made; finetuning required
+    @commands.command(name="get-loan", aliases=["gl"])    
     async def loan(self,ctx,loan_val:int):
         
-        user_data = economy_collection.find_one({'user':str(ctx.message.author)})
+        user_data = economy_collection.find_one({'user': ctx.author.id})
         if loan_val < user_data['bank'] * 2 and user_data["debt"] == 0:
-            response = discord.Embed(title=str(ctx.message.author),description=f"You took a loan of {loan_val}!",colour=discord.Colour.red()) # red bc u did a dum dum
+            response = discord.Embed(title=str(ctx.message.author), description=f"You took a loan of {loan_val}!", colour=discord.Colour.red()) 
 
             user_data['debt'] += (loan_val + int(loan_val * 0.05))
             user_data['bank'] += loan_val
             
-            economy_collection.update_one({'user':str(ctx.message.author)},{"$set":user_data})              #unsure if this works
+            economy_collection.update_one({'user': ctx.author.id}, {"$set":user_data})              
             if ctx.message.channel.name in channels_available: await ctx.message.channel.send(content=None,embed=response)
 
-            await asyncio.sleep(64800)                                  #checks if debt has been repaid, if not sends reminder
-            user_data = economy_collection.find_one({'user':str(ctx.message.author)})
+            await asyncio.sleep(64800)           
+                                   #checks if debt has been repaid, if not sends reminder
+                                   #Search for a better option than asyncio.sleep
+
+            user_data = economy_collection.find_one({'user': ctx.author.id})
             if user_data['debt'] != 0:                      
                 await ctx.message.author.send("You're about to default on your loan")
             else:
                 return
             
             await asyncio.sleep(21600)
-            user_data = economy_collection.find_one({'user':str(ctx.message.author)})
+
+            user_data = economy_collection.find_one({'user': ctx.author.id})
             if user_data['debt'] != 0:
                 user_data['debt'] = 0
                 user_data['cash'] -= (loan_val + int(loan_val * 0.1))
-                economy_collection.update_one({'user':str(ctx.message.author)},{"$set":user_data})
+                economy_collection.update_one({'user': ctx.author.id}, {"$set": user_data})
                 await ctx.message.author.send(f"poopi you messed up big time")
         else:
             await ctx.message.channel.send("You cannot take a loan greater than twice your current balance / you have an unpaid loan, repay it and try again.")
 
-    @commands.command(name = "repay-loan", aliases = ["rl"])
+
+    #rewritten to use IDs till here.
+    @commands.command(name="repay-loan", aliases=["rl"])
     async def repay_loan(self,ctx):
         user_data = economy_collection.find_one({'user':str(ctx.message.author)})
         if user_data['debt'] > 0:
