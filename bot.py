63afff287633a265c86fcf33ca428a0e5a02cf3f
Anand Krishna <40204976+anand2312@users.noreply.github.com>
2020-11-26 13:00:20 +0400

    Bump version to 2.0a

--- bot.py
+++ bot.py
@@ -45,6 +45,8 @@ bot.notes_collection = db["notes"]
 bot.bday_collection = db["bday"]
 bot.pins_collection = db["pins"]
 bot.role_names_collection = db["role"]
+
+bot.__version__ = "2.0a"
  
 @bot.event
 async def on_ready(): # sends this message when bot starts working in #bot-tests
