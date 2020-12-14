fee5ff1664ee26d4c3d62391e153776fca3f2ac4
Anand Krishna <anandkrishna2312@gmail.com>
2020-12-14 09:30:15 +0400

    Error fix
    Figure object not iterable, seek won't work

--- utils/graphing.py
+++ utils/graphing.py
@@ -67,8 +67,7 @@ def _make_graph(title: str, *, xlabel: str, ylabel: str ,x_axis: np.array, y_axi
     buffer = io.BytesIO()
     fig.savefig(buffer, format="png", bbox_inchex="tight")     # saves file with name <date>-<first plotted hour>-<last plotted hour>
     plt.close(fig)
-    buffer.seek(0)
-
+    
     return buffer
 
 def make_discord_embed(image_buffer: io.BytesIO) -> ImageEmbed:
