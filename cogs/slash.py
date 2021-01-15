ea9469776ac1364566a3d1c3838f92a70cfd2c2f
Anand Krishna <anandkrishna2312@gmail.com>
2021-01-15 14:35:44 +0400

    Trying out slash ext commands

---
+++ cogs/slash.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+from discord_slash import cog_ext, SlashCommand, SlashContext
+
+class CrajySlashCommands(commands.Cog):
+    def __init__(self, bot):
+        if not hasattr(bot, "slash"):
+            bot.slash = SlashCommand(bot, auto_register=True, override_type=True)
+
+        self.bot = bot
+        self.bot.slash.get_cog_commands(self
+        
+    @cog_ext.cog_subcommand(base="wat", name="use")
+    async def group_say(self, ctx: SlashContext, text: str):
+        existing = await self.bot.stupid_collection.find_one({"key": text})
+        await ctx.send(content=existing["output"])
+
+def setup(bot):
+    bot.add_cog(CrajySlashCommands(bot))
