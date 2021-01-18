fa039e33cbedd9d1c48b5f912b2b704177f4dcd9
Anand Krishna <anandkrishna2312@gmail.com>
2021-01-18 11:19:52 +0400

    Subclassed commands.Bot, commands.Context

---
+++ internal/bot.py
@@ -0,0 +1,104 @@
+import os
+import random
+from collections import defaultdict
+import datetime
+
+import discord
+from discord.ext import commands, tasks
+
+import asyncio
+from aiohttp import ClientSession
+
+from random import choice
+from secret.TOKEN import TOKEN
+from secret.constants import *
+from utils.timezone import BOT_TZ
+from utils.embed import CrajyEmbed
+from utils.enumerations import EmbedType
+
+from internal.help_class import HelpCommand
+from internal.context import CrajyContext
+
+
+class CrajyBot(commands.Bot):
+    """Subclass of commands.Bot with some attributes set."""
+    def __init__(self, *args, **kwargs):
+        self.db_pool = kwargs.get("db")     # instance of a connection pool has to be passed in while instantiating.
+        self.http_session = ClientSession()
+        self.chat_money_cache = defaultdict(lambda: 0)
+        self.__version__ = "3.0a"
+        kwargs.pop("db")
+        super().__init__(*args, **kwargs)
+
+    async def on_ready():
+        embed = CrajyEmbed(embed_type=EmbedType.BOT, description="Ready!")
+        embed.quick_set_author(self.user)
+        embed.timestamp = datetime.datetime.utcnow()
+        await self.get_channel(BOT_ANNOUNCE_CHANNEL).send(embed=embed)
+
+    async def on_message(message):
+        """Cache the message counts before adding cash to users balance in the database."""
+        if message.author.bot:
+            return  
+        if message.channel.id in CHAT_MONEY_CHANNELS:
+            self.chat_money_cache[message.author.id] += 1
+        await self.process_commands(message)
+
+    async def on_member_join(member):
+        """When a new member joins, add them to the database and greet them in DMs."""
+        await self.register_new_member(member)
+        embed = CrajyEmbed(embed_type=EmbedType.SUCCESS, title="Hi! Welcome to Crajy!")
+        embed.quick_set_author(member)
+        embed.quick_set_footer(self.user)
+        await member.send(embed=embed)
+
+    async def on_member_remove(member):
+        """When a member leaves, quietly remove them from the database."""
+        await self.delete_member(member)
+
+    async def on_command_error(ctx, error):
+        """Global error handler."""
+        embed = CrajyEmbed(embed_type=EmbedType.FAIL, title="Command Errored.")
+        embed.quick_set_footer(self.user)
+
+        if isinstance(error, commands.CommandOnCooldown):
+            embed.description = F"{ctx.author.nick}, you have to wait {int(error.retry_after/60)} minutes before using this command again."
+            return await ctx.send(embed=embed)
+        elif isinstance(error, commands.CommandNotFound):
+            if ctx.message.content.startswith(".."):
+                pass
+            else:
+                embed.description = f"Command {ctx.invoked_with} not found."
+                return await ctx.send(embed=embed)
+        else:
+            embed.title = "Unexpected error occurred."
+            embed.description = str(error)
+            return await ctx.send(embed=embed)
+            raise error
+
+    async def get_context(self, message, *, cls=None):
+        """Overriding get_context to use custom context."""
+        return await super().get_context(message, cls=CrajyContext)
+
+    async def process_commands(self, message):
+        """Triggers typing in channels before sending a message."""
+        if message.author.bot:
+            return
+
+        ctx = await self.get_context(message)
+        if ctx.valid and getattr(ctx.cog, "qualified_name", None) != "Jishaku":
+            await ctx.trigger_typing()
+        await self.invoke(ctx)
+
+    async def register_new_member(member):
+        async with self.db_pool.acquire() as connection:
+            async with connection.transaction():
+                await connection.execute("INSERT INTO economy VALUES($1)", member.id)
+                await connection.execute("INSERT INTO user_details VALUES($1)", member.id)
+                await connection.execute("INSERT INTO inventories VALUES($1)", member.id)
+        
+    async def delete_member(member):
+        async with self.db_pool.acquire() as connection:
+            async with connection.transaction():
+                await connection.execute("DELETE FROM economy WHERE user_id=$1", member.id)
+    
--- internal/context.py
+++ internal/context.py
@@ -1,1 +1,28 @@
 # reimplement a custom context that lazily pulls data from db, and maybe-reply.
+from discord.ext import commands
+import contextlib
+from internal.enumerations import Table
+
+class CrajyContext(commands.Context):
+    """Custom context with two added features:
+        - maybe_reply: replies to a message if there's a message between invocation message and reply
+        - DB interaction; to easily get data from the db; the context has methods built to fetch the ctx.author's data.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__dict__.update(dict.fromkeys(["waiting", "result", "channel_used", "running", "failed"]))
+
+    async def author_data(self, table: Table) -> dict:
+        async with self.bot.db_pool.acquire() as connection:
+            async with connection.transaction():
+                ...
+
+    async def maybe_reply(self, content=None, mention_author=False, **kwargs):
+        """Replies if there is a message in between the command invoker and the bot's message."""
+        await asyncio.sleep(0.05)
+        with contextlib.suppress(discord.HTTPException):
+            if getattr(self.channel,"last_message", False) != self.message:
+                return await self.reply(content, mention_author=mention_author, **kwargs)
+        await self.send(content, **kwargs)
+
+    # get column keys for dictify from the table enum
