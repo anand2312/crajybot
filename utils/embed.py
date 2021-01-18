e8c59a7841de07f1dd65e835186106e012d2b4c0
Anand Krishna <anandkrishna2312@gmail.com>
2021-01-18 11:20:02 +0400

    Subclassed Embed

--- utils/embed.py
+++ utils/embed.py
@@ -1,25 +1,11 @@
 import discord
 import datetime
-import enum
-
-
-class EmbedType(enum.Enum):
-    FAIL = 1
-    SUCCESS = 2
-    WARNING = 3
-    BOT = 4
 
 
 class CrajyEmbed(discord.Embed):
     def __init__(self, embed_type: EmbedType, **kwargs) -> None:
         super().__init__(**kwargs)
-        mapping = {
-            EmbedType.FAIL: 0xFF0000,
-            EmbedType.SUCCESS: 0x32CD32,
-            EmbedType.WARNING: 0xFFFF00,
-            EmbedType.BOT: 0xA9A9A9
-            }
-        self.colour = mapping[embed_type]
+        self.colour = embed_type.value
         self.timestamp = datetime.datetime.utcnow()
 
     def quick_set_author(member: discord.Member) -> None:
