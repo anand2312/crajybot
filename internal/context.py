cc9a4552c6cf95421d36406f27e3280ae86adc5b
Anand Krishna <anandkrishna2312@gmail.com>
2021-01-18 11:34:47 +0400

    Completed ctx.user_data

--- internal/context.py
+++ internal/context.py
@@ -1,4 +1,5 @@
 # reimplement a custom context that lazily pulls data from db, and maybe-reply.
+import discord
 from discord.ext import commands
 import contextlib
 from internal.enumerations import Table
@@ -8,14 +9,14 @@ class CrajyContext(commands.Context):
         - maybe_reply: replies to a message if there's a message between invocation message and reply
         - DB interaction; to easily get data from the db; the context has methods built to fetch the ctx.author's data.
     """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.__dict__.update(dict.fromkeys(["waiting", "result", "channel_used", "running", "failed"]))
+    async def user_data(self, table: Table, member: discord.Member = None) -> dict:
+        """Returns `user`s data from the `table` specified.
+        `user` defaults to ctx.author.
+        The data returned is in an instance of asyncpg.Record - a tuple/dict hybrid, and can use both indexing and key lookup."""
+        if member is None:
+            member = self.author
 
-    async def author_data(self, table: Table) -> dict:
-        async with self.bot.db_pool.acquire() as connection:
-            async with connection.transaction():
-                ...
+        return await self.bot.db_pool.fetchrow(f"SELECT * FROM {table.name} WHERE user_id=$1", member.id)
 
     async def maybe_reply(self, content=None, mention_author=False, **kwargs):
         """Replies if there is a message in between the command invoker and the bot's message."""
@@ -24,5 +25,3 @@ class CrajyContext(commands.Context):
             if getattr(self.channel,"last_message", False) != self.message:
                 return await self.reply(content, mention_author=mention_author, **kwargs)
         await self.send(content, **kwargs)
-
-    # get column keys for dictify from the table enum
