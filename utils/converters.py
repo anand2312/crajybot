b42ed1e81100222be098ed2a3fcafb03b802c0da
Anand Krishna <anandkrishna2312@gmail.com>
2021-01-21 11:37:33 +0400

    Fix typehint

--- utils/converters.py
+++ utils/converters.py
@@ -1,8 +1,9 @@
-  
+from datetime import timedelta  
+
 from discord.ext import commands
 from utils import timezone
 
 class CustomTimeConverter(commands.Converter):
     """Returns a timedelta object."""
-    async def convert(self, ctx, arg: str) -> timezone.Timedelta:
+    async def convert(self, ctx, arg: str) -> timedelta:
         return timezone.get_timedelta(arg)
