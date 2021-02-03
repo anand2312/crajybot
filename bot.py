dfdf1c2f05b7b9c3080877c653c18045f07eae3a
Rishabh Chopde <65121903+Rishabh69420@users.noreply.github.com>
2021-02-03 12:24:47 +0400

    Made stock loop correction
    stock price will increase/decrease if the last 2-4 price changes are negative/positive respectively

--- bot.py
+++ bot.py
@@ -22,6 +22,8 @@ bot = CrajyBot(command_prefix=commands.when_mentioned_or("."),
 
 bot.task_loops = dict()    # add all task loops, across cogs to this.
 
+last_4 = []                # List for stock loop correction
+
 @bot.listen("on_message")
 async def chat_money_tracker(message):
     if message.author.bot:
@@ -35,12 +37,31 @@ async def stock_price():
     message_channel = bot.get_channel(BOT_ANNOUNCE_CHANNEL)
 
     rand_sign = random.choice(["+","-"])
+    pos = 0
+    neg = 0
     if rand_sign == "+":
         rand_val = random.randint(1,6)
         emb_type = EmbedType.SUCCESS
     else:
         rand_val = -random.randint(1,6)
         emb_type = EmbedType.FAIL
+
+    last_4.append(rand_val)
+	if len(last_4) > 4:
+		del(last_4[0])
+    
+    for i in last_4:
+    	if i<0:
+        	neg += 1
+      	else:
+        	pos += 1
+
+    if pos >= random.randint(2,4):
+      	rand_val = -(rand_val)
+		emb_type = EmbedType.FAIL
+	if neg >= random.randint(2,4):
+      	rand_val = -(rand_val)
+		emb_type = EmbedType.SUCCESS
 
     new = await bot.db_pool.fetchval(f"UPDATE shop SET price=price + $1 WHERE item_name='stock' RETURNING price", rand_val)
 
@@ -52,27 +73,6 @@ async def stock_price():
 @stock_price.before_loop
 async def stock_price_before():
     await bot.wait_until_ready()
-    
-@stock_price.error
-async def stock_price_error(error):
-  """In case the loop tries taking the stock price below 15, increase the price OR keep it at the same price."""
-  if isinstance(error, asyncpg.exceptions.CheckViolationError):
-    message_channel = bot.get_channel(BOT_ANNOUNCE_CHANNEL)
-    # copy pasted the same process 
-    rand_sign = random.choice(["+","-"])
-    if rand_sign == "+":
-        rand_val = random.randint(1,6)
-        emb_type = EmbedType.SUCCESS
-    else:
-        rand_val = -random.randint(1,6)
-        emb_type = EmbedType.FAIL
-
-    new = await bot.db_pool.fetchval(f"UPDATE shop SET price=price + $1 WHERE item_name='stock' RETURNING price", rand_val)
-
-    embed = CrajyEmbed(title="Stock Price Updated!", embed_type=emb_type)
-    embed.description = f"New price: {new}"
-    embed.quick_set_author(bot.user)
-    await message_channel.send(embed=embed)
 
 @tasks.loop(hours=24)
 async def birthday_loop():
