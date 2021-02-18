47c4d7364b6ab39196366debc472dbab92b32f2f
Anand Krishna <anandkrishna2312@gmail.com>
2021-02-18 21:24:18 +0400

    Black - utils

--- utils/converters.py
+++ utils/converters.py
@@ -1,17 +1,19 @@
-from datetime import timedelta  
+from datetime import timedelta
 
 from discord.ext import commands
 from utils import timezone
 
+
 class CustomTimeConverter(commands.Converter):
     """Returns a timedelta object."""
+
     async def convert(self, ctx, arg: str) -> timedelta:
         return timezone.get_timedelta(arg)
-    
-    
+
+
 class KwargConverter(commands.Converter):
     """Takes a string kwarg and returns a dict.
     This has to be used along with Greedy and dict merging for multiple kwargs."""
+
     async def convert(self, ctx, arg: str) -> dict:
         return dict([arg.split("=")])
-    
