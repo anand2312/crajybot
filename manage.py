e499b92b9d26517dba3ca143263bd735d7374eab
anand2312 <40204976+anand2312@users.noreply.github.com>
2022-03-17 08:53:24 +0400

    No exts means all exts

--- manage.py
+++ manage.py
@@ -11,13 +11,11 @@ parser.add_argument("-e", "--exts", help="List of extensions to load", nargs="*"
 
 args = parser.parse_args()
 
-func = lambda x: f"exts.{x}"
-exts = list(map(func, args.exts))
-
-
 logger.info(f"Starting bot")
 
-if len(exts) == 0:
+if args.exts is None:
     asyncio.run(main())
 else:
+    func = lambda x: f"exts.{x}"
+    exts = list(map(func, args.exts))
     asyncio.run(main(exts))
